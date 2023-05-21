255c5f02bf8c32255330427f46dad8054ab212de
Marcus20119 <hoanglongnguyenhuu20119@gmail.com>
2023-05-21 22:21:39 +0700

    add realtime write

--- src/config/realtime.js
+++ src/config/realtime.js
@@ -13,6 +13,7 @@ export const initRealtime = conn_plc => {
   setInterval(() => {
     console.log('-----------READ DATA FROM PLC-------------');
     conn_plc.readAllItems((error, values) => {
+      console.log('values:', values);
       if (error) {
         console.log('Error occurred when reading data: ');
       }
@@ -33,23 +34,121 @@ export const initRealtime = conn_plc => {
     console.log('Done writing.');
   }
 
-  onValue(ref(realTimeDb, 'XLNT_WEB'), snapshot => {
+  // onValue(ref(realTimeDb, 'XLNT_WEB'), snapshot => {
+  //   const data = snapshot.val();
+  //   // Không ghi dữ liệu khi khởi tạo lần đầu
+  //   if (write_value !== null) {
+  //     // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
+  //     if (isEqual(write_value, data)) {
+  //       console.log('===> DATA NOT CHANGED');
+  //     } else {
+  //       console.log('===> DATA CHANGED');
+  //       console.log('-----------WRITE DATA TO PLC-------------');
+  //       conn_plc.writeItems(
+  //         Object.keys(data),
+  //         Object.values(data),
+  //         valuesWritten
+  //       );
+  //     }
+  //     write_value = { ...data };
+  //   }
+  // });
+  onValue(ref(realTimeDb, 'XLNT_WEB/Reset_T_Sum_Pump1'), snapshot => {
     const data = snapshot.val();
-    // Không ghi dữ liệu khi khởi tạo lần đầu
     if (write_value !== null) {
       // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
-      if (isEqual(write_value, data)) {
-        console.log('===> DATA NOT CHANGED');
+      if (isEqual(write_value.Reset_T_Sum_Pump1, data)) {
+        console.log('===> Reset_T_Sum_Pump1 NOT CHANGED');
       } else {
-        console.log('===> DATA CHANGED');
+        console.log('===> Reset_T_Sum_Pump1 CHANGED');
         console.log('-----------WRITE DATA TO PLC-------------');
-        conn_plc.writeItems(
-          Object.keys(data),
-          Object.values(data),
-          valuesWritten
-        );
+        conn_plc.writeItems('Reset_T_Sum_Pump1', data, valuesWritten);
       }
-      write_value = { ...data };
+      write_value.Reset_T_Sum_Pump1 = data;
+    }
+  });
+  onValue(ref(realTimeDb, 'XLNT_WEB/Reset_T_Sum_Pump2'), snapshot => {
+    const data = snapshot.val();
+    if (write_value !== null) {
+      // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
+      if (isEqual(write_value.Reset_T_Sum_Pump2, data)) {
+        console.log('===> Reset_T_Sum_Pump2 NOT CHANGED');
+      } else {
+        console.log('===> Reset_T_Sum_Pump2 CHANGED');
+        console.log('-----------WRITE DATA TO PLC-------------');
+        conn_plc.writeItems('Reset_T_Sum_Pump2', data, valuesWritten);
+      }
+      write_value.Reset_T_Sum_Pump2 = data;
+    }
+  });
+  onValue(ref(realTimeDb, 'XLNT_WEB/Reset_T_Sum_Fan'), snapshot => {
+    const data = snapshot.val();
+    if (write_value !== null) {
+      // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
+      if (isEqual(write_value.Reset_T_Sum_Fan, data)) {
+        console.log('===> Reset_T_Sum_Fan NOT CHANGED');
+      } else {
+        console.log('===> Reset_T_Sum_Fan CHANGED');
+        console.log('-----------WRITE DATA TO PLC-------------');
+        conn_plc.writeItems('Reset_T_Sum_Fan', data, valuesWritten);
+      }
+      write_value.Reset_T_Sum_Fan = data;
+    }
+  });
+  onValue(ref(realTimeDb, 'XLNT_WEB/T_On_Pump_Min'), snapshot => {
+    const data = snapshot.val();
+    if (write_value !== null) {
+      // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
+      if (isEqual(write_value.T_On_Pump_Min, data)) {
+        console.log('===> T_On_Pump_Min NOT CHANGED');
+      } else {
+        console.log('===> T_On_Pump_Min CHANGED');
+        console.log('-----------WRITE DATA TO PLC-------------');
+        conn_plc.writeItems('T_On_Pump_Min', data, valuesWritten);
+      }
+      write_value.T_On_Pump_Min = data;
+    }
+  });
+  onValue(ref(realTimeDb, 'XLNT_WEB/T_Off_Pump_Min'), snapshot => {
+    const data = snapshot.val();
+    if (write_value !== null) {
+      // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
+      if (isEqual(write_value.T_Off_Pump_Min, data)) {
+        console.log('===> T_Off_Pump_Min NOT CHANGED');
+      } else {
+        console.log('===> T_Off_Pump_Min CHANGED');
+        console.log('-----------WRITE DATA TO PLC-------------');
+        conn_plc.writeItems('T_Off_Pump_Min', data, valuesWritten);
+      }
+      write_value.T_Off_Pump_Min = data;
+    }
+  });
+  onValue(ref(realTimeDb, 'XLNT_WEB/T_On_Fan_Min'), snapshot => {
+    const data = snapshot.val();
+    if (write_value !== null) {
+      // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
+      if (isEqual(write_value.T_On_Fan_Min, data)) {
+        console.log('===> T_On_Fan_Min NOT CHANGED');
+      } else {
+        console.log('===> T_On_Fan_Min CHANGED');
+        console.log('-----------WRITE DATA TO PLC-------------');
+        conn_plc.writeItems('T_On_Fan_Min', data, valuesWritten);
+      }
+      write_value.T_On_Fan_Min = data;
+    }
+  });
+  onValue(ref(realTimeDb, 'XLNT_WEB/T_Off_Fan_Min'), snapshot => {
+    const data = snapshot.val();
+    if (write_value !== null) {
+      // Vì mỗi lần dữ liệu thay đổi, snapshot sẽ chạy 2 lần để so sánh, chỉ ghi dữ liệu khi bắt được dữ liệu mới
+      if (isEqual(write_value.T_Off_Fan_Min, data)) {
+        console.log('===> T_Off_Fan_Min NOT CHANGED');
+      } else {
+        console.log('===> T_Off_Fan_Min CHANGED');
+        console.log('-----------WRITE DATA TO PLC-------------');
+        conn_plc.writeItems('T_Off_Fan_Min', data, valuesWritten);
+      }
+      write_value.T_Off_Fan_Min = data;
     }
   });
 };
